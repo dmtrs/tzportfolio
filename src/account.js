49d6be4615f6b85f6b159149876cb210960d6424
Dmtrs Flaco Meng <tydeas.dr@gmail.com>
2021-11-05 20:35:46 +0200

    feat(account): Temporary remove graphs

--- src/account.js
+++ src/account.js
@@ -2,22 +2,33 @@ import { define, store, html } from 'hybrids';
 import { Account, AccountBalanceHistory } from './schema/accounts.js';
 import './atoms/mutez.js'
 
+import 'carbon-web-components/es/components/tile/tile.js';
 import './graph.js';
 
 define({
   tag: "tzp-account",
   address: 'tz1TmBz5CkiMrrcpxATTg1nDtd1CUoBd17DC',
   account: store(Account, 'address'),
-  balance_history: store(AccountBalanceHistory, 'address'),
-  render: ({ account, balance_history }) => html`
-    <div>
+  render: ({ account  }) => html`
+    <bx-tile>
       ${store.pending(account) && `Loading...`}
       ${store.error(account) && `Something went wrong...`}
 
       ${store.ready(account) && html`
-        <p>${account.address} ${account.alias} <tzp-mutez v="${account.balance}"></tzp-mutez></p>
+        <div>
+          <span>${account.address}</span><div>${account.alias}</div>
+          <tzp-mutez v="${account.balance}"></tzp-mutez>
+        </div>
       `}
-    </div>
+    </bx-tile>
+    `
+});
+
+define({
+  tag: "tzp-account-graph",
+  address: 'tz1TmBz5CkiMrrcpxATTg1nDtd1CUoBd17DC',
+  balance_history: store(AccountBalanceHistory, 'address'),
+  render: ({ balance_history }) => html`
     <div>
       ${store.pending(balance_history) && `Loading...`}
       ${store.error(balance_history) && `Something went wrong...`}
